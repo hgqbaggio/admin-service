dae0920d6bad9c95b4beaf1a74c1154094d97b3e
gqhudl <gqhudl@cn.ibm.com>
2020-04-01 17:51:06 +0800

    new fsd project

---
+++ src/main/java/com/ibm/fsd/admin/AdminServiceTestApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.fsd.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/fsd/admin/AdminServiceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.fsd.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
